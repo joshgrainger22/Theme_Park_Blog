eeb1e3cc966c5ace8bfb6d3bca31fee90c0b2881
Dan Ventura <danice.ventura@gmail.com>
2022-04-06 07:36:01 -0600

    added LoadAttractions to actions file

--- client/src/store/actions/CitiesAttracAction.js
+++ client/src/store/actions/CitiesAttracAction.js
@@ -1,18 +1,31 @@
 import { GET_CITIES, GET_CITIES_ATTRACTIONS } from '../types'
 import { GetCities } from '../../services/CitiesAttracService'
-
-
+import { GetAttractions } from '../../services/CitiesAttracService'
 
 export const LoadCities = () => {
-    return async (dispatch) => {
-        try {
-            const cities = await GetCities()
-            dispatch({
-                type: GET_CITIES,
-                payload:cities
-            })
-        } catch (error) {
-            throw error
-        }
+  return async (dispatch) => {
+    try {
+      const cities = await GetCities()
+      dispatch({
+        type: GET_CITIES,
+        payload: cities
+      })
+    } catch (error) {
+      throw error
     }
+  }
+}
+
+export const LoadAttractions = () => {
+  return async (dispatch) => {
+    try {
+      const attractions = await GetAttractions()
+      dispatch({
+        type: GET_CITIES_ATTRACTIONS,
+        payload: attractions
+      })
+    } catch (error) {
+      throw error
+    }
+  }
 }
